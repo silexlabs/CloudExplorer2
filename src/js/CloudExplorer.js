be402c235e09a7bc71fcbcbcf8ff3412ddeffafd
M48 <M48@imac-de-m48.home>
2016-07-19 20:58:27 +0200

    some style

--- src/js/CloudExplorer.js
+++ src/js/CloudExplorer.js
@@ -53,48 +53,58 @@ export default class CloudExplorer extends React.Component {
   }
   render() {
     return <div>
-    <h2>Services:</h2>
-      <ServiceSelector
-        service={this.state.service}
-        services={this.state.services}
-        onChange={(selection) => this.setService(selection)}
-      />
-      <h2>Buttons:</h2>
-      <ButtonBar
-        service={this.state.service}
-        selection={this.state.selection}
-        onRename={(newName) => console.dir(`rename "${this.state.selection[0].name}" to "${newName}"`)}
-        onCreateFolder={(newName) => console.log(`create the folder "${newName}" in "${this.state.path.join('/')}"`)}
-        onReload={() => this.reload()}
-        onDownload={() => console.log(`Download`, this.state.selection)}
-        onUpload={() => console.log('Upload', this.state.selection, this.state.path.join('/'))}
-        onDelete={() => console.log('Delete', this.state.selection)}
-      />
-      <ButtonConfirm
-        service={this.state.service}
-        selection={this.state.selection}
-        pickFolder={this.state.pickFolder}
-        onPick={() => this.props.onPick(this.state.selection)}
-        onEnter={folder => this.cd([folder.name], true)}
-        onCancel={() => this.cancel()}
-      />
-      <h2>Breadcrumbs:</h2>
-      <Breadcrumbs
-        service={this.state.service}
-        path={this.state.path}
-        onEnter={path => this.cd(path)}
-      />
-      <h2>Files:</h2>
-      <Files
-        path={this.state.path}
-        selection={this.state.selection}
-        files={this.state.files}
-        onChange={(selection) => this.setState({selection: selection})}
-        onEnter={folder => this.cd([folder.name], true)}
-        onPick={(file) => this.props.onPick(file)}
-      />
-      <h2>Dialogs:</h2>
-      <ModalDialog />
+      <div className="services panel">
+        <h2>Services</h2>
+        <ServiceSelector
+          service={this.state.service}
+          services={this.state.services}
+          onChange={(selection) => this.setService(selection)}
+        />
+      </div>
+      <div className="buttons panel">
+        <h2>Buttons</h2>
+        <ButtonBar
+          service={this.state.service}
+          selection={this.state.selection}
+          onRename={(newName) => console.dir(`rename "${this.state.selection[0].name}" to "${newName}"`)}
+          onCreateFolder={(newName) => console.log(`create the folder "${newName}" in "${this.state.path.join('/')}"`)}
+          onReload={() => this.reload()}
+          onDownload={() => console.log(`Download`, this.state.selection)}
+          onUpload={() => console.log('Upload', this.state.selection, this.state.path.join('/'))}
+          onDelete={() => console.log('Delete', this.state.selection)}
+        />
+        <ButtonConfirm
+          service={this.state.service}
+          selection={this.state.selection}
+          pickFolder={this.state.pickFolder}
+          onPick={() => this.props.onPick(this.state.selection)}
+          onEnter={folder => this.cd([folder.name], true)}
+          onCancel={() => this.cancel()}
+        />
+      </div>
+      <div className="breadcrumbs panel">
+        <h2>Breadcrumbs</h2>
+        <Breadcrumbs
+          service={this.state.service}
+          path={this.state.path}
+          onEnter={path => this.cd(path)}
+        />
+      </div>
+      <div className="files panel">
+        <h2>Files</h2>
+        <Files
+          path={this.state.path}
+          selection={this.state.selection}
+          files={this.state.files}
+          onChange={(selection) => this.setState({selection: selection})}
+          onEnter={folder => this.cd([folder.name], true)}
+          onPick={(file) => this.props.onPick(file)}
+        />
+      </div>
+      <div className="dialogs panel">
+        <h2>Dialogs</h2>
+        <ModalDialog />
+      </div>
     </div>;
   }
 }
