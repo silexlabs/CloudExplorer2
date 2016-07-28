6264184a37ecd1a15f1d286b394dfe76cdc07bd5
Alex Hoyau <a.hoyau@silexlabs.org>
2016-07-28 03:09:28 +0200

    browser history, UP button, download button

--- src/js/ButtonConfirm.js
+++ src/js/ButtonConfirm.js
@@ -6,10 +6,12 @@ export default class ButtonConfirm extends React.Component {
   render() {
     this.allowPick = this.props.service && this.props.selection.length === 1 && this.props.pickFolder === this.props.selection[0].is_dir;
     this.allowEnter = this.props.service && this.props.selection.length === 1 && this.props.selection[0].is_dir;
+    this.allowUp = this.props.service && this.props.path.length > 0;
     return <section className="button-confirm">
       <ul>
         <li onClick={(e) => this.allowPick && this.props.onPick(this.props.selection[0])} className={this.allowPick ? "enabled" : "disabled"}>Pick</li>
         <li onClick={(e) => this.allowEnter && this.props.onEnter(this.props.selection[0])} className={this.allowEnter ? "enabled" : "disabled"}>Enter</li>
+        <li onClick={(e) => this.allowUp && this.props.onUp(this.props.selection[0])} className={this.allowUp ? "enabled" : "disabled"}>Up</li>
         <li onClick={(e) => this.props.onCancel()} className="enabled">Cancel</li>
       </ul>
     </section>;
