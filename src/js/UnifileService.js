f49f24e5d76ff4a6996126f41fd7e0f6bfa4eb96
jotakar <jcgare@gmail.com>
2018-10-13 00:35:13 +0200

    Cange Unifile for modal login
    Modify to use a modal window in non OAuth login as Ftp, Sftp. Other uses a blank window in browser, so user known he is exiting from sitebuilder and useing an external storage service.

--- src/js/UnifileService.js
+++ src/js/UnifileService.js
@@ -192,7 +192,15 @@ export default class UnifileService {
         this.authEnded(serviceName, resolve, reject);
       } else {
         // Open a blank window right away, before we know the URL, otherwise the browser blocks it
-        const win = window.open();
+        var winLog ;
+        //For OAuth (github, droopbox...) we use a blank window, as the user is exiting from the site, so it's more evident
+        //Non OAuth, like ftp, sftp uses the modal window. OAuth uses blank window browser.
+        if (service.isOAuth)
+            winLog = window.open();
+        else
+            winLog = modalLog('Login','about:blank');
+        
+        const win = winLog;
         const req = new XMLHttpRequest();
         req.open('POST', `${UnifileService.ROOT_URL}${serviceName}/authorize`);
         req.onload = () => {
